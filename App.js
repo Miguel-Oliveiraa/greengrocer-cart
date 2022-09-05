e32e5806acd1eeeaf3a9ed6d43ed2446c21b36a4
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-09-05 11:42:46 -0300

    src: google fonts

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { SafeAreaView, StatusBar } from "react-native";
-import Cesta from "./src/Cesta";
+import { SafeAreaView, StatusBar, View } from "react-native";
+import Cesta from "./src/telas/Cesta";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import mock from "./src/mocks/cesta";
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <StatusBar />
-      <Cesta />
-    </SafeAreaView>
-  );
+  const [fontloaded] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold,
+  });
+  if (!fontloaded) {
+    return <View />;
+  } else {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar />
+        <Cesta {...mock} />
+      </SafeAreaView>
+    );
+  }
 }
